947790740f77ee16c867ccfe93620e9a2a7fd706
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2022-06-10 23:35:05 +0300

    Added navbar, start work witn main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Main from "./components/Main";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <Navigation />
+      <Main />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+const Main = () => (
+  <main>
+    <div className="p-3">
+      <h1 className="text-center">TITLE</h1>
+      <p className="text-center">
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+        aut?
+      </p>
+    </div>
+  </main>
+);
+
+export default Main;
